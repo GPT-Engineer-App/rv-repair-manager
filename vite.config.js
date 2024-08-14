78e84d63f4a38a8515f1acb0f999787b6337ce08
MarkW1919 <mark.williamson119@gmail.com>
2024-08-14 12:36:02 -0500

    refactor

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,14 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { resolve } from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath } from 'url';
 
-// https://vitejs.dev/config/
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+
 export default defineConfig({
-  server: {
-    host: "::",
-    port: "8080",
-  },
   plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: "@",
-        replacement: fileURLToPath(new URL("./src", import.meta.url)),
-      },
-      {
-        find: "lib",
-        replacement: resolve(__dirname, "lib"),
-      },
-    ],
-  },
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 });
